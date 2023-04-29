2bd5a158109827184c66b581fb4d649805bf9d43
Artem Onufrei <onufrei.artem@gmail.com>
2023-04-29 20:44:58 +0200

    Fixed income pipe

--- src/app/income.pipe.ts
+++ src/app/income.pipe.ts
@@ -5,17 +5,17 @@ export class IncomePipe implements PipeTransform {
   transform(value: number): string {
     let amount = value / 1000000000; // B
     if (amount > 1) {
-      return `${Math.trunc(amount)}B`;
+      return `${amount.toFixed(1)}B`;
     }
 
     amount = value / 1000000; // M
     if (amount > 1) {
-      return `${Math.trunc(amount)}M`;
+      return `${amount.toFixed(1)}M`;
     }
 
     amount = value / 1000; // K
     if (amount > 1) {
-      return `${Math.trunc(amount)}K`;
+      return `${amount.toFixed(1)}K`;
     }
 
     return `${value}`;
